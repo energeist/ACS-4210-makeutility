df04df87323d33b19c54b118962e136c6971a647
Mark Rattle <mark.rattle@gmail.com>
2024-05-07 09:37:07 -0400

    seed database with gameMap objects

--- client/client.go
+++ client/client.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"net/http"
 
 	"github.com/energeist/tournament-calculator/helpers"
-	"github.com/energeist/tournament-calculator/models"
 )
 
 func main() {
@@ -20,24 +16,27 @@ func main() {
 	fmt.Println("API Key: ", APIKey)
 
 	// Create a new player
-	player := models.Player{
-		Name:      "Test Player",
-		Rating:    1000,
-		VsProtoss: 50,
-		VsTerran:  50,
-		VsZerg:    50,
-	}
+	// player := models.Player{
+	// 	Name:      "Test Player",
+	// 	Rating:    1000,
+	// 	VsProtoss: 50,
+	// 	VsTerran:  50,
+	// 	VsZerg:    50,
+	// }
 
-	playerJSON, err := json.Marshal(player)
-	if err != nil {
-		fmt.Println("Error marshaling player to JSON: ", err)
-		return
-	}
+	// playerJSON, err := json.Marshal(player)
+	// if err != nil {
+	// 	fmt.Println("Error marshaling player to JSON: ", err)
+	// 	return
+	// }
 
-	resp, err := http.Post("http://localhost:"+serverPort+"/player", "application/json", bytes.NewBuffer(playerJSON))
-	if err != nil {
-		fmt.Println("Error creating player: ", err)
-	}
+	// resp, err := http.Post("http://localhost:"+serverPort+"/player", "application/json", bytes.NewBuffer(playerJSON))
+	// if err != nil {
+	// 	fmt.Println("Error creating player: ", err)
+	// }
 
-	fmt.Println("Response: ", resp)
+	// fmt.Println("Response: ", resp)
+
+	// Seed the db with maps
+	helpers.GenerateDBSeed()
 }
