08bded271c56925a86cb0eaa0978436627e64406
Mark Rattle <mark.rattle@gmail.com>
2024-05-08 08:44:50 -0400

    now checks to see if top X players are already seeded

--- client/client.go
+++ client/client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"strconv"
 
 	"github.com/energeist/tournament-calculator/helpers"
 	"github.com/energeist/tournament-calculator/models"
@@ -16,41 +17,48 @@ func main() {
 	serverPort := helpers.LoadFromDotEnv("GIN_PORT")
 	APIKey := helpers.LoadFromDotEnv("ALIGULAC_API_KEY")
 
-	fmt.Println("Server Port: ", serverPort)
+	// Get top X players from Aligulac API
+	topXPlayers := 50
 
-	// Get top 50 players from Aligulac API
-	url := "http://aligulac.com/api/v1/player/?current_rating__isnull=false&order_by=-current_rating__rating&limit=3&apikey=" + APIKey
-	multiplePlayersData, err := helpers.GetRequest(url)
+	existingPlayersData, err := helpers.GetRequest(helpers.ServerURL("player", serverPort))
 	if err != nil {
 		fmt.Println("Error: ", err)
 		return
 	}
 
-	fmt.Println("Raw API response with mutliple players: ")
-	fmt.Println(string(multiplePlayersData))
-
-	var APIResponsePlayers APIResponsePlayers
-	if err := json.Unmarshal(multiplePlayersData, &APIResponsePlayers); err != nil {
+	var existingPlayers []models.Player
+	if err := json.Unmarshal(existingPlayersData, &existingPlayers); err != nil {
 		fmt.Println("Error: ", err)
 		return
 	}
 
-	fmt.Println("Parsed API response with multiple players: ")
-	fmt.Println(APIResponsePlayers)
+	if len(existingPlayers) < topXPlayers {
+		url := "http://aligulac.com/api/v1/player/?current_rating__isnull=false&order_by=-current_rating__rating&limit=" + strconv.Itoa(topXPlayers) + "&apikey=" + APIKey
 
-	fmt.Println("APIResponsePlayers.Objects: ")
-	fmt.Println(APIResponsePlayers.Objects)
-	fmt.Println("APIResponsePlayers.Objects[0]: ")
-	fmt.Println(APIResponsePlayers.Objects[0])
-
-	// Store each player in the database
-	for _, player := range APIResponsePlayers.Objects {
-		response, err := helpers.PostRequest(helpers.ServerURL("player", serverPort), player)
+		multiplePlayersData, err := helpers.GetRequest(url)
 		if err != nil {
-			fmt.Println("Error storing player:", err)
-
+			fmt.Println("Error: ", err)
+			return
 		}
-		fmt.Println("Response from storing player: ", response)
+
+		var APIResponsePlayers APIResponsePlayers
+		if err := json.Unmarshal(multiplePlayersData, &APIResponsePlayers); err != nil {
+			fmt.Println("Error: ", err)
+			return
+		}
+
+		fmt.Println("APIResponsePlayers.Objects[0]: ")
+		fmt.Println(APIResponsePlayers.Objects[0])
+
+		// Store each player in the database
+		for _, player := range APIResponsePlayers.Objects {
+			_, err := helpers.PostRequest(helpers.ServerURL("player", serverPort), player)
+			if err != nil {
+				fmt.Println("Error storing player:", err)
+			}
+		}
+	} else {
+		fmt.Println("Top " + strconv.Itoa(topXPlayers) + " players already seeded")
 	}
 }
 
